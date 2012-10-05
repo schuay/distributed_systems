ffe8507d99e601c1b5eec45864e03d9f630cca6f
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-05 13:24:15 +0200

    Add argument parsing to client

--- 1b/src/com/ds/Client.java
+++ 1b/src/com/ds/Client.java
@@ -2,12 +2,51 @@ package com.ds;
 
 public class Client {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ParsedArgs parsedArgs = null;
+		try {
+			parsedArgs = new ParsedArgs(args);
+			System.out.printf("Host: %s TCP Port: %d UDP Port: %d%n",
+					parsedArgs.getHost(),
+					parsedArgs.getTcpPort(),
+					parsedArgs.getUdpPort());
+		} catch (IllegalArgumentException e) {
+			System.err.printf("Usage: java %s <host> <tcpPort> <udpPort>%n",
+					Client.class.getName());
+			return;
+		}
+	}
+}
 
+class ParsedArgs {
+	private final String host;
+	private int tcpPort;
+	private int udpPort;
+
+	public ParsedArgs(String[] args) {
+		if (args.length != 3) {
+			throw new IllegalArgumentException();
+		}
+
+		host = args[0];
+
+		try {
+			tcpPort = Integer.parseInt(args[1]);
+			udpPort = Integer.parseInt(args[2]);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
 	}
 
+	public String getHost() {
+		return host;
+	}
+
+	public int getTcpPort() {
+		return tcpPort;
+	}
+
+	public int getUdpPort() {
+		return udpPort;
+	}
 }
