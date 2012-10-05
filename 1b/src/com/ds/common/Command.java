eb83280cc3de5fd6951a8b6102b57642589fdffc
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-05 18:21:35 +0200

    Move Command to com.ds.common

--- 1b/src/com/ds/common/Command.java
+++ 1b/src/com/ds/common/Command.java
@@ -1,4 +1,4 @@
-package com.ds;
+package com.ds.common;
 
 import java.util.StringTokenizer;
 
--- 1b/src/com/ds/server/ServerThread.java
+++ 1b/src/com/ds/server/ServerThread.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import com.ds.Command;
+import com.ds.common.Command;
 
 public class ServerThread implements Runnable {
 
