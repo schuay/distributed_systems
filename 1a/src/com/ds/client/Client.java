3d7708990410b76ac7042effb0888376ef73e15e
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-05 15:13:04 +0200

    Fix 1A (password is obfuscated)

--- 1a/src/com/ds/client/Client.java
+++ 1a/src/com/ds/client/Client.java
@@ -8,35 +8,38 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	
+
 	private static final String HOST = "stockholm.vitalab.tuwien.ac.at";
 	private static final int PORT = 9000;
+	private static final String USER = "dslab115";
+	private static final String PASSWORD = "xxxxxxxx";
 
 	public static void main(String[] args) throws IOException {
 		Socket socket = null;
 		PrintWriter out = null;
 		BufferedReader in = null;
-		
+
 		try {
 			socket = new Socket(HOST, PORT);
 			out = new PrintWriter(socket.getOutputStream());
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		} catch (UnknownHostException e) {
 			System.err.printf("Don't know about host: %s.%n", HOST);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.printf("Couldn't get I/O for the connection to: %s: %s%n",
-            		HOST, e.getMessage());
-            System.exit(1);
-        }
-		
-		out.printf("!login %s %s");
-		
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.printf("Couldn't get I/O for the connection to: %s: %s%n",
+					HOST, e.getMessage());
+			System.exit(1);
+		}
+
+		out.printf("!login %s %s%n", USER, PASSWORD);
+		out.flush();
+
 		String response;
 		while ((response = in.readLine()) != null) {
 			System.out.println(response);
 		}
-		
+
 		out.close();
 		in.close();
 		socket.close();
