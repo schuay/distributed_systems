969e88576ab8f81581e7923e668115e48551bd20
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-05 14:41:23 +0200

    Shutdown on Enter

--- 1b/src/com/ds/server/Server.java
+++ 1b/src/com/ds/server/Server.java
@@ -6,9 +6,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Server {
+public class Server implements Runnable {
 
-	public static void main(String[] args) throws IOException {
+	private static volatile boolean listening = true;
+	private static ServerSocket serverSocket = null;
+
+	public static void main(String[] args) {
 		ParsedArgs parsedArgs = null;
 		try {
 			parsedArgs = new ParsedArgs(args);
@@ -18,7 +21,6 @@ public class Server {
 			return;
 		}
 
-		ServerSocket serverSocket = null;
 		try {
 			serverSocket = new ServerSocket(parsedArgs.getTcpPort());
 		} catch (IOException e) {
@@ -26,9 +28,26 @@ public class Server {
 			return;
 		}
 
+		Thread thread = new Thread(new Server());
+		thread.start();
+
+		System.out.println("Server started, press Enter to initiate shutdown.");
+
+		/* Initialization is done. We will not accept new connections until server shutdown
+		 * is triggered.
+		 */
+
 		int id = 0;
 		ExecutorService executorService = Executors.newCachedThreadPool();
-		executorService.submit(new ServerThread(id++, serverSocket.accept()));
+		while (listening) {
+			try {
+				executorService.submit(new ServerThread(id++, serverSocket.accept()));
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+
+		/* Shutdown gracefully. */
 
 		try {
 			executorService.shutdown();
@@ -37,7 +56,32 @@ public class Server {
 			System.err.println(e.getMessage());
 		}
 
-		serverSocket.close();
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	/* Prevent other classes from creating a Server instance. */
+	private Server() { }
+
+	/**
+	 * Waits for the user to press Enter, and then triggers shutdown.
+	 */
+	@Override
+	public void run() {
+		try {
+			int in;
+			do {
+				in = System.in.read();
+			} while (in != '\n');
+
+			listening = false;
+			serverSocket.close();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
 
