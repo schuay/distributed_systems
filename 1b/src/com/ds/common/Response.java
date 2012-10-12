e219794f2f9d8e805454fd3b737a29e6d9254e73
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-12 16:03:55 +0200

    Create basic Response class

---
+++ 1b/src/com/ds/common/Response.java
@@ -0,0 +1,30 @@
+package com.ds.common;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+
+	private static final long serialVersionUID = 7978041566542954342L;
+
+	private final Rsp response;
+	public Rsp getResponse() {
+		return response;
+	}
+
+	public enum Rsp {
+		OK,
+		ERROR,
+		AUCTION_LIST,
+		AUCTION_CREATED,
+		BID
+	}
+
+	public Response(Rsp response) {
+		this.response = response;
+	}
+
+	@Override
+	public String toString() {
+		return response.toString();
+	}
+}
