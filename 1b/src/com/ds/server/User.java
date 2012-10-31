316c7565d1afd4de9bed54203f58f61162aca5d3
Jakob Gruber <jakob.gruber@gmail.com>
2012-10-31 12:10:05 +0100

    Fix a bug in which User objects are incorrectly replaced

--- 1b/src/com/ds/server/User.java
+++ 1b/src/com/ds/server/User.java
@@ -6,48 +6,50 @@ import java.util.List;
 
 public class User {
 
-	public static final User NONE = new User("none");
+    public static final User NONE = new User("none");
 
-	private final String name;
-	public String getName() {
-		return name;
-	}
+    private final String name;
+    public String getName() {
+        return name;
+    }
 
-	private boolean loggedIn = false;
-	public boolean isLoggedIn() {
-		return loggedIn;
-	}
+    private boolean loggedIn = false;
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
 
-	private final List<String> notifications = new ArrayList<String>();
-	private int port;
-	private InetAddress address;
+    private final List<String> notifications = new ArrayList<String>();
+    private int port;
+    private InetAddress address;
 
-	public User(String name) {
-		this.name = name;
-	}
+    public User(String name) {
+        this.name = name;
+    }
 
-	public void postNotification(String message) {
-		notifications.add(message);
-		if (loggedIn) {
-			processNotifications();
-		}
-	}
+    public void postNotification(String message) {
+        System.out.printf("Posting UDP Notification: %s%n", message);
+        notifications.add(message);
+        if (loggedIn) {
+            processNotifications();
+        }
+    }
 
-	private void processNotifications() {
-		for (String msg : notifications) {
-			Server.sendUdp(address, port, msg);
-		}
-		notifications.clear();
-	}
+    private void processNotifications() {
+        for (String msg : notifications) {
+            System.out.printf("Sending UDP Notification: %s%n", msg);
+            Server.sendUdp(address, port, msg);
+        }
+        notifications.clear();
+    }
 
-	public void login(InetAddress address, int port) {
-		this.address = address;
-		this.port = port;
-		this.loggedIn = true;
-		processNotifications();
-	}
+    public void login(InetAddress address, int port) {
+        this.address = address;
+        this.port = port;
+        this.loggedIn = true;
+        processNotifications();
+    }
 
-	public void logout() {
-		this.loggedIn = false;
-	}
+    public void logout() {
+        this.loggedIn = false;
+    }
 }
