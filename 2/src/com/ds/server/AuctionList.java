d54d18c7d80fca46fb17bd2be8b0d8e4b0cee321
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-02 12:36:47 +0100

    Add event listeners to AuctionList
    AuctionEvents are already triggered.

--- 2/src/com/ds/server/AuctionList.java
+++ 2/src/com/ds/server/AuctionList.java
@@ -1,20 +1,27 @@
 
 package com.ds.server;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.ds.event.AuctionEvent;
+import com.ds.event.Event;
 
 public class AuctionList {
 
     private final ConcurrentHashMap<Integer, Auction> auctions = new ConcurrentHashMap<Integer, Auction>();
     private final Timer timer = new Timer();
     private int id = 0;
+    private final List<EventListener> listeners = new ArrayList<EventListener>();
 
     public synchronized int add(String description, User owner, Date end) {
         int auctionId = id++;
 
+        notifyListeners(new AuctionEvent(AuctionEvent.AUCTION_STARTED, auctionId));
         auctions.put(auctionId, new Auction(auctionId, description, owner, end));
         timer.schedule(new AuctionTimerTask(this, auctionId), end);
 
@@ -30,6 +37,7 @@ public class AuctionList {
     }
 
     private synchronized void expire(int auctionId) {
+        notifyListeners(new AuctionEvent(AuctionEvent.AUCTION_ENDED, auctionId));
         auctions.remove(auctionId);
     }
 
@@ -47,12 +55,38 @@ public class AuctionList {
     }
 
     /**
+     * Listeners are notified of all auction related events.
+     */
+    public interface EventListener {
+        void onEvent(Event event);
+    }
+
+    public void addOnEventListener(EventListener listener) {
+        synchronized (listeners) {
+            listeners.add(listener);
+        }
+    }
+
+    public void removeOnEventListener(EventListener listener) {
+        synchronized (listeners) {
+            listeners.remove(listener);
+        }
+    }
+
+    private void notifyListeners(Event event) {
+        synchronized (listeners) {
+            for (EventListener listener : listeners) {
+                listener.onEvent(event);
+            }
+        }
+    }
+
+    /**
      * Responsible for ending an auction once it expires.
      */
     private static class AuctionTimerTask extends TimerTask {
 
         private final int id;
-
         private final AuctionList list;
 
         public AuctionTimerTask(AuctionList list, int id) {
