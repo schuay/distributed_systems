10bb4140933dfd30d1e972e78fbee434cc98ed3b
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-02 09:39:59 +0100

    Adapt server argument parsing to new specification

--- 2/src/com/ds/server/ParsedArgs.java
+++ 2/src/com/ds/server/ParsedArgs.java
@@ -1,21 +1,33 @@
 package com.ds.server;
 
 class ParsedArgs {
-	private final int tcpPort;
+    private final int tcpPort;
+    private final String analyticsBindingName;
+    private final String billingBindingName;
 
-	public ParsedArgs(String[] args) {
-		if (args.length != 1) {
-			throw new IllegalArgumentException();
-		}
+    public ParsedArgs(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException();
+        }
 
-		try {
-			tcpPort = Integer.parseInt(args[0]);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException();
-		}
-	}
+        try {
+            tcpPort = Integer.parseInt(args[0]);
+            analyticsBindingName = args[1];
+            billingBindingName = args[2];
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 
-	public int getTcpPort() {
-		return tcpPort;
-	}
+    public int getTcpPort() {
+        return tcpPort;
+    }
+
+    public String getAnalyticsBindingName() {
+        return analyticsBindingName;
+    }
+
+    public String getBillingBindingName() {
+        return billingBindingName;
+    }
 }
