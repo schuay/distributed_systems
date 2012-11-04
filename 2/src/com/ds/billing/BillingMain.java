5fb84c65240f9358637f2578f10fba2905825fe9
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-04 19:40:29 +0100

    Empty Billing and Management main classes

---
+++ 2/src/com/ds/billing/BillingMain.java
@@ -0,0 +1,13 @@
+package com.ds.billing;
+
+public class BillingMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ 2/src/com/ds/management/ManagementMain.java
@@ -0,0 +1,13 @@
+package com.ds.management;
+
+public class ManagementMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
