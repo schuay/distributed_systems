4e857fa195fdf83cd4fc179e42745686e65f9cc3
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-06 17:34:10 +0100

    Get started on analytics tracking and generation

--- 2/src/com/ds/analytics/AnalyticsServer.java
+++ 2/src/com/ds/analytics/AnalyticsServer.java
@@ -2,6 +2,7 @@ package com.ds.analytics;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -16,6 +17,7 @@ import com.ds.loggers.Log;
 public class AnalyticsServer implements Analytics {
 
     private final Subscriptions subs = new Subscriptions();
+    private final AnalyticsData data = new AnalyticsData();
 
     @Override
     public String subscribe(String regex, EventProcessor callback) throws RemoteException {
@@ -30,7 +32,7 @@ public class AnalyticsServer implements Analytics {
 
         /* Generate derived events. */
 
-        List<Event> generatedEvents = generateEvents(event);
+        List<Event> generatedEvents = data.updateAndGenerateEvents(event);
 
         /* Notify subscribers. */
 
@@ -41,9 +43,71 @@ public class AnalyticsServer implements Analytics {
 
     }
 
-    private List<Event> generateEvents(Event event) {
-        // TODO Auto-generated method stub
-        return new ArrayList<Event>();
+    private static class AnalyticsData {
+
+        private static final long MSECS_PER_SEC = 1000;
+        private static final long SECS_PER_MIN = 60;
+
+        private final int sessionSecondsMin = -1;
+        private final int sessionSecondsMax = -1;
+        private final int sessionSecondsAccumulated = 0;
+        private final int sessionCount = 0;
+
+        private final double bidPriceMax = -1;
+        private final int bidCount = 0;
+
+        private final int auctionSecondsAccumulated = 0;
+        private final int auctionCount = 0;
+        private final int auctionSuccessCount = 0;
+
+        private final Date runningSince;
+
+
+        public AnalyticsData() {
+            runningSince = new Date();
+        }
+
+        public List<Event> updateAndGenerateEvents(Event event) {
+
+            /* TODO: keep track of users, auctions, and generate events accordingly. */
+
+            return new ArrayList<Event>();
+        }
+
+        private int getSessionSecondsMin() {
+            return sessionSecondsMin;
+        }
+
+        private int getSessionSecondsMax() {
+            return sessionSecondsMax;
+        }
+
+        private double getSessionSecondsAvg() {
+            return (double)sessionSecondsAccumulated / sessionCount;
+        }
+
+        private double getBidPriceMax() {
+            return bidPriceMax;
+        }
+
+        private double getBidCountPerMinute() {
+            Date now = new Date();
+            long seconds = (now.getTime() - runningSince.getTime()) / MSECS_PER_SEC;
+
+            if (seconds == 0) {
+                return 0;
+            }
+
+            return (bidCount * SECS_PER_MIN) / (double)seconds;
+        }
+
+        private double getAuctionSecondsAvg() {
+            return (double)auctionSecondsAccumulated / auctionCount;
+        }
+
+        private double getAuctionSuccessRatio() {
+            return (double)auctionSuccessCount / auctionCount;
+        }
     }
 
     @Override
