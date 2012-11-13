07837b14757d7bbb636e694c77447b966f5587b3
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-13 09:55:56 +0100

    Fix incomplete StatisticsEvent ids

--- 2/src/com/ds/event/StatisticsEvent.java
+++ 2/src/com/ds/event/StatisticsEvent.java
@@ -5,12 +5,12 @@ public class StatisticsEvent extends Event {
     private static final long serialVersionUID = 1L;
 
     public static final String USER_SESSIONTIME_MIN = "USER_SESSIONTIME_MIN";
-    public static final String USER_SESSIONTIME_MAX = "USER_";
-    public static final String USER_SESSIONTIME_AVG = "USER_";
-    public static final String BID_PRICE_MAX = "USER_";
-    public static final String BID_COUNT_PER_MINUTE = "USER_";
-    public static final String AUCTION_TIME_AVG = "USER_";
-    public static final String AUCTION_SUCCESS_RATIO = "USER_";
+    public static final String USER_SESSIONTIME_MAX = "USER_SESSIONTIME_MAX";
+    public static final String USER_SESSIONTIME_AVG = "USER_SESSIONTIME_AVG";
+    public static final String BID_PRICE_MAX = "BID_PRICE_MAX";
+    public static final String BID_COUNT_PER_MINUTE = "BID_COUNT_PER_MINUTE";
+    public static final String AUCTION_TIME_AVG = "AUCTION_TIME_AVG";
+    public static final String AUCTION_SUCCESS_RATIO = "AUCTION_SUCCESS_RATIO";
 
     private final double value;
 
