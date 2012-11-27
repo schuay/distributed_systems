7a07040211ce177eb03168f09a6036bf3711218c
c553db14 <c553db14@example.com>
2012-11-27 04:55:30 +0100

    Add default price steps

--- 2/src/com/ds/billing/PriceStepStorage.java
+++ 2/src/com/ds/billing/PriceStepStorage.java
@@ -16,6 +16,14 @@ class PriceStepStorage {
 
     private final TreeSet<PriceStep> storage = new TreeSet<PriceStep>();
 
+    private PriceStepStorage() {
+        insert(new PriceStep(0, 100, 3, 7));
+        insert(new PriceStep(100, 200, 5, 6.5));
+        insert(new PriceStep(200, 500, 7, 6));
+        insert(new PriceStep(500, 1000, 10, 5.5));
+        insert(new PriceStep(1000, 0, 15, 5));
+    }
+
     synchronized PriceSteps getPriceSteps() {
         return new PriceSteps(new ArrayList<PriceStep>(storage));
     }
