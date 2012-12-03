0ff3f08a5af380d76702da5e8a1a44c3a02e5bf3
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-03 20:32:20 +0100

    Add all provided code snippets to SecurityUtils

---
+++ 3/src/com/ds/util/SecurityUtils.java
@@ -0,0 +1,136 @@
+package com.ds.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.openssl.PasswordFinder;
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Hex;
+
+import com.ds.loggers.Log;
+
+public class SecurityUtils {
+
+    public static byte[] toBase64(byte[] from) {
+        return Base64.encode(from);
+    }
+
+    public static byte[] fromBase64(byte[] from) {
+        return Base64.decode(from);
+    }
+
+    public static PrivateKey readPrivateKey(String path) throws IOException {
+        PEMReader in = null;
+
+        try {
+            in = new PEMReader(new FileReader(path), new PasswordFinder() {
+                @Override
+                public char[] getPassword() {
+                    char[] password = null;
+                    try {
+                        // reads the password from standard input for decrypting the private key
+                        System.out.println("Enter pass phrase:");
+                        password = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+                    } catch (IOException e) {
+                        Log.e(e.getLocalizedMessage());
+                    }
+
+                    return password;
+                }
+            });
+
+            KeyPair keyPair = (KeyPair) in.readObject();
+            return keyPair.getPrivate();
+        } finally {
+            try { in.close(); } catch (Throwable t) { }
+        }
+    }
+
+    public static PublicKey readPublicKey(String path) throws IOException {
+        PEMReader in = null;
+
+        try {
+            in = new PEMReader(new FileReader(path));
+            return (PublicKey)in.readObject();
+        } finally {
+            try { in.close(); } catch (Throwable t) { }
+        }
+    }
+
+    public static byte[] getSecureRandom() {
+        // generates a 32 byte secure random number
+        SecureRandom secureRandom = new SecureRandom();
+        final byte[] number = new byte[32];
+        secureRandom.nextBytes(number);
+        return number;
+    }
+
+
+    public static SecretKey generateSecretKey(int keysize) throws NoSuchAlgorithmException {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        // KEYSIZE is in bits
+        generator.init(keysize);
+        return generator.generateKey();
+    }
+
+    public static Cipher getCipher(String algorithm, int mode, Key key, SecureRandom iv)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        // make sure to use the right ALGORITHM for what you want to do
+        // (see text)
+        Cipher crypt = Cipher.getInstance(algorithm);
+        // MODE is the encryption/decryption mode
+        // KEY is either a private, public or secret key
+        // IV is an init vector, needed for AES
+        crypt.init(mode, key, iv);
+        return crypt;
+    }
+
+    public static Key readSecretKey(String path, String algorithm) throws IOException {
+        byte[] keyBytes = new byte[1024];
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream(path);
+            fis.read(keyBytes);
+        } finally {
+            fis.close();
+        }
+
+        byte[] input = Hex.decode(keyBytes);
+        // make sure to use the right ALGORITHM for what you want to do
+        // (see text)
+        return new SecretKeySpec(input, algorithm);
+    }
+
+    public static byte[] getHMAC(Key key, String algorithm, byte[] message) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac hMac = Mac.getInstance(algorithm);
+        hMac.init(key);
+        hMac.update(message);
+        return hMac.doFinal();
+    }
+
+    public static boolean verifyHMAC(Key key, String algorithm, byte[] hmac, byte[] message) throws InvalidKeyException, NoSuchAlgorithmException {
+        byte[] computedHash = getHMAC(key, algorithm, message);
+        return MessageDigest.isEqual(computedHash, hmac);
+    }
+
+}
