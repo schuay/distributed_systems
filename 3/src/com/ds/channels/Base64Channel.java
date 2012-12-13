dc054e98d030a8025e96ac1634e9313f3274bfc0
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-13 10:14:02 +0100

    Do not extend TcpChannel in Base64Channel

--- 3/src/com/ds/channels/Base64Channel.java
+++ 3/src/com/ds/channels/Base64Channel.java
@@ -1,30 +1,34 @@
 package com.ds.channels;
 
 import java.io.IOException;
-import java.net.Socket;
 
 import com.ds.util.SecurityUtils;
 
-public class Base64Channel extends TcpChannel {
+public class Base64Channel implements Channel {
 
-    public Base64Channel(Socket socket) throws IOException {
-        super(socket);
+    private final Channel channel;
+
+    public Base64Channel(Channel channel) throws IOException {
+        this.channel = channel;
     }
 
     @Override
     public void write(byte[] bytes) throws IOException {
         byte[] encoded = SecurityUtils.toBase64(bytes);
-        super.write(encoded);
+        channel.write(encoded);
     }
 
     @Override
     public String readLine() throws IOException {
-        return new String(read(), CHARSET);
+        return new String(read(), TcpChannel.CHARSET);
     }
 
     @Override
     public byte[] read() throws IOException {
-        String encoded = super.readLine();
+        String encoded = channel.readLine();
         return SecurityUtils.fromBase64(encoded.getBytes());
     }
+
+    @Override
+    public void close() { }
 }
