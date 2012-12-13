a13a13fc37c6ec9ce8d5ffde83c9d55df8ff53ab
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-13 08:53:48 +0100

    Adapt SecureChannel

--- 3/src/com/ds/channels/SecureChannel.java
+++ 3/src/com/ds/channels/SecureChannel.java
@@ -1,40 +1,56 @@
 package com.ds.channels;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
-//public class SecureChannel implements StringChannel {
-//
-//    private final Base64Channel base64Channel;
-//    private final Cipher encCipher;
-//    private final Cipher decCipher;
-//
-//    public SecureChannel(Base64Channel base64Channel, Key sessionKey, SecureRandom iv)
-//            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-//        this.base64Channel = base64Channel;
-//        this.encCipher = SecurityUtils.getCipher(SecurityUtils.AES, Cipher.ENCRYPT_MODE,
-//                sessionKey, iv);
-//        this.decCipher = SecurityUtils.getCipher(SecurityUtils.AES, Cipher.DECRYPT_MODE,
-//                sessionKey, iv);
-//    }
-//
-//    @Override
-//    public void printf(String format, Object... args) {
-//        String sendStr = String.format(format, args);
-//        base64Channel.printBytes(encCipher.update(sendStr.getBytes()));
-//    }
-//
-//    @Override
-//    public String readLine() throws IOException {
-//        return new String(decCipher.update(base64Channel.readBytes()));
-//    }
-//
-//    @Override
-//    public void close() {
-//        try {
-//            encCipher.doFinal();
-//        } catch (Exception e) {}
-//
-//        try {
-//            decCipher.doFinal();
-//        } catch (Exception e) {}
-//    }
-//}
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
+import com.ds.loggers.Log;
+import com.ds.util.SecurityUtils;
+
+
+public class SecureChannel implements Channel {
+
+    private final Base64Channel base64Channel;
+    private final Cipher encCipher;
+    private final Cipher decCipher;
+
+    public SecureChannel(Base64Channel base64Channel, Key sessionKey, SecureRandom iv)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        this.base64Channel = base64Channel;
+        this.encCipher = SecurityUtils.getCipher(SecurityUtils.AES, Cipher.ENCRYPT_MODE,
+                sessionKey, iv);
+        this.decCipher = SecurityUtils.getCipher(SecurityUtils.AES, Cipher.DECRYPT_MODE,
+                sessionKey, iv);
+    }
+
+    @Override
+    public void write(byte[] bytes) throws IOException {
+        byte[] encrypted = encCipher.update(bytes);
+        base64Channel.write(encrypted);
+    }
+
+    @Override
+    public String readLine() throws IOException {
+        return new String(read(), TcpChannel.CHARSET);
+    }
+
+    @Override
+    public byte[] read() throws IOException {
+        return decCipher.update(base64Channel.read());
+    }
+
+    @Override
+    public void close() {
+        try {
+            encCipher.doFinal();
+            decCipher.doFinal();
+        } catch (Exception e) {
+            Log.e(e.getMessage());
+        }
+    }
+}
