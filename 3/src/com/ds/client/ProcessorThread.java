7c8d221df50fc68e5e2e06f1d07f8acc72754deb
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-16 20:57:55 +0100

    Further work on client states and terminal processing

--- 3/src/com/ds/client/ProcessorThread.java
+++ 3/src/com/ds/client/ProcessorThread.java
@@ -1,15 +1,21 @@
 package com.ds.client;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import java.security.PrivateKey;
 import java.util.concurrent.BlockingQueue;
 
+import com.ds.channels.Base64Channel;
 import com.ds.channels.Channel;
 import com.ds.channels.NopChannel;
+import com.ds.channels.RsaChannel;
 import com.ds.commands.Command;
+import com.ds.commands.CommandLogin;
 import com.ds.loggers.Log;
+import com.ds.util.SecurityUtils;
 
 public class ProcessorThread implements Runnable {
 
@@ -17,7 +23,7 @@ public class ProcessorThread implements Runnable {
     private final BlockingQueue<Parcel> q; /* For convenience. */
     private boolean keepGoing = true;
     private final PrintWriter out;
-    private final Channel channel = new NopChannel();
+    private Channel channel = new NopChannel();
 
     public ProcessorThread(Socket socket, Data data) throws IOException {
         this.out = new PrintWriter(socket.getOutputStream());
@@ -28,7 +34,7 @@ public class ProcessorThread implements Runnable {
     @Override
     public void run() {
         try {
-            State state = new State();
+            State state = new StateLoggedOut();
 
             while (keepGoing) {
                 Parcel parcel = null;
@@ -66,10 +72,21 @@ public class ProcessorThread implements Runnable {
         }
     }
 
+    private static PrivateKey readPrivateKey(String clientKeyDir, String user) throws IOException {
+        File dir = new File(clientKeyDir);
+        File file = new File(dir, String.format("%s.pem", user));
+
+        if (!file.exists() || !file.isFile()) {
+            throw new IllegalArgumentException("Could not read private key");
+        }
+
+        return SecurityUtils.readPrivateKey(file.getAbsolutePath());
+    }
+
     /* The default state, which is responsible for parcels that should
      * be handled the same way in every state.
      */
-    private class State {
+    private abstract class State {
 
         public State processNetworkParcel(Parcel parcel) {
             System.out.println(parcel.getMessage());
@@ -91,11 +108,72 @@ public class ProcessorThread implements Runnable {
                 send(cmd.toString());
                 break;
             default:
-                System.out.println(parcel.getMessage());
+                send(cmd.toString());
                 break;
             }
 
             return this;
         }
+    }
+
+    private class StateLoggedOut extends State {
+
+        @Override
+        public State processTerminalParcel(Parcel parcel) {
+            Command cmd = null;
+            try {
+                cmd = Command.parse(parcel.getMessage());
+            } catch (IllegalArgumentException e) {
+                Log.e("Invalid command: %s", parcel.getMessage());
+                return this;
+            }
+
+            switch (cmd.getType()) {
+            case LOGIN:
+                try {
+                    CommandLogin c = (CommandLogin)cmd;
+
+                    /* Encrypt the login command with the server's public key. */
+
+                    PrivateKey key = readPrivateKey(data.getClientKeyDir(), c.getUser());
+                    Channel b64c = new Base64Channel(new NopChannel());
+                    Channel rsac = new RsaChannel(b64c, data.getServerKey(), key);
+
+                    channel = rsac;
+
+                    send(c.toString());
+                    return new StateChallenge();
+                } catch (Throwable t) {
+                    Log.e(t.getMessage());
+                    return this;
+                }
+            default:
+                return super.processTerminalParcel(parcel);
+            }
+        }
+
+        private class StateChallenge extends State {
+
+            @Override
+            public State processTerminalParcel(Parcel parcel) {
+                Command cmd = null;
+                try {
+                    cmd = Command.parse(parcel.getMessage());
+                } catch (IllegalArgumentException e) {
+                    Log.e("Invalid command: %s", parcel.getMessage());
+                    return this;
+                }
+
+                switch (cmd.getType()) {
+                case LOGOUT:
+                    send(cmd.toString());
+                    channel = new NopChannel();
+                    return new StateLoggedOut();
+                default:
+                    return super.processTerminalParcel(parcel);
+                }
+            }
+        }
+
     }
 }
