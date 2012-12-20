653f8e8c82283177093cb86512da278efcdbeb8a
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-20 14:58:34 +0100

    Remove unused code from GroupBidMonitor

--- 3/src/com/ds/server/GroupBidMonitor.java
+++ 3/src/com/ds/server/GroupBidMonitor.java
@@ -1,11 +1,9 @@
 package com.ds.server;
 
+import java.util.Date;
+
 import com.ds.commands.CommandConfirm;
 import com.ds.server.UserList.User;
-
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * The GroupBidMonitor monitors the current state of group bids
@@ -15,13 +13,10 @@ import java.util.Set;
 class GroupBidMonitor {
     /* Currently only 3 is supported. */
     public static final int GROUP_SIZE = 3;
-    //public static final long FAIRNESS_TIME = 300000;
     public static final long FAIRNESS_TIME = 999999999;
 
     private final AuctionList auctions;
     private final UserList users;
-
-    private final Set<User> groupBidUsers = new HashSet<User>();
 
     public GroupBidMonitor(AuctionList auctions, UserList users) {
         this.auctions = auctions;
@@ -39,7 +34,7 @@ class GroupBidMonitor {
             listener.onRejected();
             return;
         }
-        
+
         /* If there is just one free user remaining (the current one is marked blocked too)... */
         if ((users.numLoggedInUsers() - users.numBlockedUsers()) == 0) {
             /* ... only allow confirming bids we can finish. */
@@ -47,7 +42,7 @@ class GroupBidMonitor {
                 long current = new Date().getTime();
                 int maxConfirms = users.getMaxAcceptedConfirms(current);
                 if (user.getAcceptedConfirms(current) == maxConfirms ||
-                    !auctions.rejectGroupBidForConfirm(auctionId, bidder, amount, current, maxConfirms)) {
+                        !auctions.rejectGroupBidForConfirm(auctionId, bidder, amount, current, maxConfirms)) {
                     listener.onRejected();
                     return;
                 }
